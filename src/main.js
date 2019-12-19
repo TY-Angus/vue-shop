bfd9678e3cf83aebd8b30e82bd77a2e6290f1cc0
angus <2936454740@qq.com>
2019-12-19 14:22:38 +0800

    2.1完成了登录页面的静态页面布局，字体图标等

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入element-ui
+import './plugins/element.js'
+
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+// 导入全局样式表
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
