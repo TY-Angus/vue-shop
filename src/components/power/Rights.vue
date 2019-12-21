6212b06c598eec675db23394a11b97c1874e9cd3
angus <2936454740@qq.com>
2019-12-21 20:17:43 +0800

    5.1权限列表渲染完成

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <breadcrumb val1="权限管理" val2="权限列表"></breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-table :data="rightsList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="权限名称" prop="authName"></el-table-column>
+        <el-table-column label="路径" prop="path"></el-table-column>
+        <el-table-column label="权限等级" prop="level">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level==='0'">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level==='1'">二级</el-tag>
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { rightsList: [] }
+  },
+  methods: {
+    async getRightsList() {
+      const { data } = await this.$http.get('rights/list')
+      if (data.meta.status !== 200) return this.$message.error('获取权限列表失败')
+      this.rightsList = data.data
+    }
+  },
+  created() {
+    this.getRightsList()
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
