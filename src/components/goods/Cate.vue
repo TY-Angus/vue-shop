63d1cf213a36342edaba7a3347c0f455cccf59fb
angus <2936454740@qq.com>
2019-12-23 16:18:58 +0800

    6.2商品分类添加功能完成

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -8,7 +8,7 @@
       <!-- 分配分类区域 -->
       <el-row>
         <el-col>
-          <el-button type="primary">添加分类</el-button>
+          <el-button type="primary" @click="showAddCateDialog">添加分类</el-button>
         </el-col>
       </el-row>
       <!-- 表格区域 -->
@@ -55,6 +55,38 @@
         :total="total"
       ></el-pagination>
     </el-card>
+
+    <!-- 添加分类对话框 -->
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed"
+    >
+      <el-form
+        :model="addCateForm"
+        :rules="addCateFormRules"
+        ref="addCateFormRef"
+        label-width="100px"
+      >
+        <el-form-item label="分类名称：" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类：">
+          <el-cascader
+            v-model="selectedKeys"
+            :options="parentCateList"
+            :props="cascaderProps"
+            @change="parentCateChanged"
+            clearable
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -90,7 +122,30 @@ export default {
           type: 'template',
           template: 'opt'
         }
-      ]
+      ],
+      addCateDialogVisible: false,
+      //   添加分类的表单数据对象
+      addCateForm: {
+        cat_name: '',
+        cat_pid: 0,
+        cat_level: 0
+      },
+      // 规则
+      addCateFormRules: {
+        cat_name: [{ required: true, message: '请输入分类名称', trigger: 'blur' }]
+      },
+      //   父级分类列表
+      parentCateList: [],
+      //   指定级联选择器的配置对象
+      cascaderProps: {
+        expandTrigger: 'hover',
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        checkStrictly: true
+      },
+      //   选中的父级id的数组
+      selectedKeys: []
     }
   },
   methods: {
@@ -110,6 +165,56 @@ export default {
     handleCurrentChange(newPage) {
       this.queryInfo.pagenum = newPage
       this.getCateList()
+    },
+    // 添加对话框的展示
+    showAddCateDialog() {
+      this.getParentCateList()
+      this.addCateDialogVisible = true
+    },
+    // 获取父级的分类列表
+    async getParentCateList() {
+      const { data } = await this.$http.get('categories', {
+        params: {
+          type: 2
+        }
+      })
+      if (data.meta.status !== 200) return this.$message.error('获取父级分类列表失败')
+      this.parentCateList = data.data
+    },
+    // 选择项被触发了
+    parentCateChanged() {
+      // 处理添加分类的表单数据
+      if (this.selectedKeys.length > 0) {
+        // 父级分类的id
+        this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+        // 当前分类的等级
+        this.addCateForm.cat_level = this.selectedKeys.length
+        return false
+      } else {
+        // 父级分类的id
+        this.addCateForm.cat_pid = 0
+        // 当前分类的等级
+        this.addCateForm.cat_level = 0
+        return false
+      }
+    },
+    // 点击确认添加分类
+    addCate() {
+      this.$refs.addCateFormRef.validate(async valid => {
+        if (!valid) return this.$message.error('添加分类的格式不对')
+        const { data } = await this.$http.post(`categories`, this.addCateForm)
+        if (data.meta.status !== 201) return this.$message.error('添加分类失败')
+        this.$message.success('添加分类成功')
+        this.getCateList()
+        this.addCateDialogVisible = false
+      })
+    },
+    // 监听对话框的消失
+    addCateDialogClosed() {
+      this.$refs.addCateFormRef.resetFields()
+      this.selectedKeys = []
+      this.addCateForm.cat_level = 0
+      this.addCateForm.cat_pid = 0
     }
   },
   created() {
@@ -121,5 +226,8 @@ export default {
 <style lang="less" scoped>
 .treeTable {
   margin-top: 15px;
+}
+.el-cascader {
+  width: 100%;
 }
 </style>
